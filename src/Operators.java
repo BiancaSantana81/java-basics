245406813317a60e64f2c2e18db7e9f951e2efe7
bsantana <biancasantana81@hotmail.com>
2024-12-04 13:24:48 -0300

    operators in Java

---
+++ src/Operators.java
@@ -0,0 +1,17 @@
+/**
+ * Aprendendo a usar operadores em Java.
+ */
+public class Operators {
+  /**
+   * main function.
+   */
+  public static void main(String [] args) {
+
+    int number1 = 20;
+    int number2 = 22;
+
+    System.out.println(number1 + number2);
+
+  }
+
+}
