4a0d9090ac7bf6763053604b532b6bbca4cf022a
bsantana <biancasantana81@hotmail.com>
2024-12-05 11:27:33 -0300

    learning array

--- src/ExampleArray.java
+++ src/ExampleArray.java
@@ -8,10 +8,10 @@ public class ExampleArray {
    * main function.
    */
   public static void main(String[] args) {
-    char[] objValues = {'a', 'b', 'c', 'd', 'e'};
-    char[] values = new char[5];
 
-    Arrays.fill(values, 'a');
+    // Preenchendo diretamente no array:
+
+    char[] objValues = {'a', 'b', 'c', 'd', 'e'};
 
     System.out.println("Valores em objValues: ");
 
@@ -20,13 +20,19 @@ public class ExampleArray {
     }
     System.out.print("\n");
 
+    // Preenchendo com fill:
+
+    char[] values = new char[5];
+    Arrays.fill(values, 'a');
+
     System.out.println("Valores em values: ");
     for (char value : values) {
       System.out.print(value);
     }
     System.out.print("\n");
 
-    System.out.println("Valores em matrix: ");
+    // Criando e preenchendo uma matriz:
+
     char[][] matrix = new char[5][5];
 
     for (int i = 0; i < 5; i++) {
@@ -35,6 +41,7 @@ public class ExampleArray {
       }
     }
 
+    System.out.println("Valores em matrix: ");
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
         System.out.print(matrix[i][j]);
